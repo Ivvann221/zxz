34048af0f6ecaa9ed3f89d6e5476315afbc17d58
Ivvann221 <94067148+Ivvann221@users.noreply.github.com>
2024-02-09 22:57:10 +0200

    Add files via upload

---
+++ js/localization.js
@@ -0,0 +1,48 @@
+﻿document.addEventListener('DOMContentLoaded', async function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const langParam = urlParams.get('lang') || 'en';
+
+    try {
+        const response = await fetch('../localization/Site.xcstrings');
+        const translationsData = await response.json();
+
+        console.log('Translations Data:', translationsData);
+
+        const currentLangTranslations = translationsData.strings;
+
+        if (!currentLangTranslations) {
+            console.warn('Translations not found in the data.');
+            return;
+        }
+
+        console.log('Current Language Translations:', currentLangTranslations);
+
+        Object.keys(currentLangTranslations).forEach(key => {
+            console.log('Current Key:', key);
+
+            const translationUnits = currentLangTranslations[key].localizations;
+
+            console.log('Current Translation Units:', translationUnits);
+
+            if (translationUnits && translationUnits[langParam]) {
+                const translationUnit = translationUnits[langParam].stringUnit.value;
+
+                console.log('Current Translation Unit:', translationUnit);
+
+                const element = document.querySelector(`[data-translate="${key}"]`);
+                
+                console.log('Current Element:', element);
+
+                if (element) {
+                    element.textContent = translationUnit;
+                } else {
+                    console.warn(`Element not found for the key '${key}'.`);
+                }
+            } else {
+                console.warn(`Translation not found for the language '${langParam}' and key '${key}'.`);
+            }
+        });
+    } catch (error) {
+        console.error('Error loading translations:', error);
+    }
+});
---
+++ js/script.js
@@ -0,0 +1,69 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+  const bodyclick = document.querySelector('.body');
+  const headerBurger = document.querySelector('.header__burger');
+  const headerMenu = document.querySelector('.header__menu');
+  const headerOverlay = document.querySelector('.header__overlay');
+
+
+  headerBurger.addEventListener('click', function() {
+    changeClass();
+  });
+
+  headerOverlay.addEventListener('click', function() {
+    changeClass();
+  });
+
+  function changeClass() {
+    bodyclick.classList.toggle('lock');
+    headerBurger.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+    headerOverlay.classList.toggle('lock');
+  }
+
+  resizeHeight()
+  function resizeHeight(){
+      let vh = window.innerHeight;
+      const xxx = document.querySelector('.main');
+      xxx.style = `min-height: ${vh}px`;
+  }
+  window.addEventListener('resize', () => {
+      resizeHeight()
+  });
+
+});
+// tab
+const tabs = document.querySelectorAll('.tab');
+const tabContents = document.querySelectorAll('.tab-content');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const tabId = tab.dataset.tab;
+
+     
+        tabContents.forEach(content => {
+            content.classList.add('d-none');
+        });
+
+        
+        tabs.forEach(t => {
+            t.classList.remove('active');
+        });
+
+        document.getElementById(tabId).classList.remove('d-none');
+        tab.classList.add('active');
+    });
+});
+// photo click
+document.querySelectorAll('.cart img').forEach(item => {
+  item.addEventListener('click', () => {
+      const modal = document.querySelector('.modal');
+      const modalImage = document.getElementById('modal-image');
+
+      modal.style.display = 'flex';
+      modalImage.src = item.src;
+
+      modal.addEventListener('click', () => {
+          modal.style.display = 'none';
+      });
+  });
+});
